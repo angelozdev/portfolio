11e281467adc642735348f64a56a14802ed600b3
Angelo Zambrano <angelozam17@gmail.com>
2020-11-07 12:58:41 -0500

    refactor: add socialNetworks to fixtures

--- src/components/molecules/SocialNetworks/index.tsx
+++ src/components/molecules/SocialNetworks/index.tsx
@@ -3,34 +3,8 @@ import React from 'react'
 /* Styles */
 import { Icon, Container } from './styles'
 
-/* Components */
-import { GitHub, Gmail, LinkedIn, Twitter } from '@components/Icons'
-import { social } from '@src/styles/theme'
-
-const WIDTH = 20
-
-const socialNetWorks = [
-   {
-      icon: <GitHub width={WIDTH} height={WIDTH} />,
-      href: 'https://www.github.com/angelozdev',
-      color: social.GITHUB
-   },
-   {
-      icon: <Gmail width={WIDTH} height={WIDTH} />,
-      href: 'mailto:angelozam17@gmail.com',
-      color: social.GMAIL
-   },
-   {
-      icon: <LinkedIn width={WIDTH} height={WIDTH} />,
-      href: 'https://www.linkedin.com/in/angelozdev/',
-      color: social.LINKEDIN
-   },
-   {
-      icon: <Twitter width={WIDTH} height={WIDTH} />,
-      href: 'https://www.twitter.com/angelozdev',
-      color: social.TWITTER
-   }
-]
+/* Fixtures */
+import socialNetWorks from '@src/fixtures/socialNetworks'
 
 function SocialNetworks() {
    return (
